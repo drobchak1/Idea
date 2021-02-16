f2f15c03d04554ac6a3051162c53995b86fdf0ae
unknown <drobchak.iv@gmail.com>
2021-02-16 17:34:18 +0200

    bug fixes

--- projectchange/core/views.py
+++ projectchange/core/views.py
@@ -11,12 +11,13 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     page = request.args.get('page',1,type=int)
-    ideas = Idea.query.order_by(Idea.date.desc()).paginate(page=page,per_page=10)
+    # ideas = Idea.query.order_by(Idea.date.desc()).paginate(page=page,per_page=10)
+    ideas = Idea.query.join(IdeaLike).group_by(Idea.id).order_by(func.count(Idea.id).desc()).paginate(page=page,per_page=10)
     next_url = url_for('core.index', page=ideas.next_num) \
         if ideas.has_next else None
     prev_url = url_for('core.index', page=ideas.prev_num) \
         if ideas.has_prev else None
-
+    # ideas = Idea.query.order_by(Idea.likes.count().desc()).paginate(page=page,per_page=10)
     # ideas = Idea.query.join(IdeaLike).group_by(Idea.id).order_by(func.count().desc()).paginate(page=page,per_page=10)
     return render_template('index.html', ideas=ideas, next_url=next_url, prev_url=prev_url)
 
